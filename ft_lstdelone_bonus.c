1d6f08f7048463b12d56fb080a43c6749e48b0da
Ilan Bonneville <ilbonnev@MacBook-Air-de-Ilan.local>
2024-11-09 16:03:15 +0100

    Upload libft

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilbonnev <ilbonnev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 17:22:30 by ilbonnev          #+#    #+#             */
+/*   Updated: 2024/11/07 17:27:05 by ilbonnev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+	lst = NULL;
+}
